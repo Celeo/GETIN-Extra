afc707f589c41245ee40128cb36fbfe974bd5037
Celeo <celeodor@gmail.com>
2018-04-23 15:37:36 -0700

    Bringing over Client files

---
+++ client/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'standard'
+  ],
+  plugins: [
+    'vue'
+  ],
+  rules: {
+    'generator-star-spacing': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'arrow-parens': 0,
+    'no-multiple-empty-lines': 0,
+    'no-new': 0,
+    'space-before-function-paren': 0
+  }
+}
---
+++ client/src/pages/wiki/Logout.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  div.container
+    section.section
+      div.heading
+        h1.title Logging out ...
+</template>
+
+<script>
+export default {
+  async mounted() {
+    this.$store.commit('LOG_OUT')
+    window.localStorage.removeItem('token')
+    setInterval(() => {
+      window.location.href = '/'
+    }, 2000)
+  }
+}
+</script>
